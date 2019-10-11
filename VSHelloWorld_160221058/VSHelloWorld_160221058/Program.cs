0c75e19b0b42166f8c859cc17aa5d9b1ca8e16dc
diogo-silva-96 <160221058@estudantes.ips.pt>
2019-10-11 17:44:12 +0100

    change

---
+++ VSHelloWorld_160221058/VSHelloWorld_160221058/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VSHelloWorld_160221058
+{
+    class Program
+    {
+        // Comentário de teste
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("conamite");
+        }
+    }
+}
